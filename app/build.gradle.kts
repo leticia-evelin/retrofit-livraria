8a7eaa9bd0e84c1645a3a12bae013495652ab4bf
LETÍCIA EVELIN AGUIAR E SILVA <22282118@dsn127m1246727.cfp127aluno.local>
2023-09-15 10:33:35 -0300

    cadastro de categoria

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+
+
 }
--- app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria/Cadastro_Categoria.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria/Cadastro_Categoria.kt
@@ -2,10 +2,61 @@ package br.senai.sp.jandira.retrofit_api_livraria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
+import com.google.gson.JsonObject
+import kotlinx.coroutines.launch
+import retrofit2.create
 
 class Cadastro_Categoria : AppCompatActivity() {
+
+    private lateinit var apiService: ApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_categoria)
+
+        //conecta apiService com a api rest atraves da classe RetrofitHelper e seu método
+        apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
+
+        //recupera o componente gráfico de EditText
+        val txtCategoria = findViewById<EditText>(R.id.txtCategoria)
+
+        //trata a ação de clique do botão cadastrar
+        findViewById<Button>(R.id.btnCadastrarCategoria).setOnClickListener{
+
+            //recuperar o dado digitado pelo usuário
+            val nomeCategoria = txtCategoria.text
+
+            //enviar a requisição de cadastro para a API
+            createCategory(nomeCategoria.toString())
+        }
     }
+
+    //implementação do método createCategory
+    private fun createCategory(nome_categoria: String){
+        lifecycleScope.launch {
+
+            //montagem do corpo de dados em json
+            val body = JsonObject().apply {
+                addProperty("nome_categoria", nome_categoria)
+            }
+
+            //envio da requisição de cadastro de categoria
+            val result = apiService.createCategory(body)
+
+            //verificando a resposta da requisição
+            if (result.isSuccessful){
+                val msg = result.body()?.get("mensagemStatus")
+                Log.e("CREATE-CATEGORY", "STATUS: ${msg}")
+            } else {
+                Log.e("CREATE-CATEGORY", "ERROR: ${result.message()}")
+            }
+        }
+
+    }
+
+
 }
